3a50040415a1ad9b458e76b5d134e51466b6f321
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-02-28 15:39:37 -0700

    Make visual to show two D6 die rolling

--- dice_visual.py
+++ dice_visual.py
@@ -3,35 +3,37 @@ from plotly import offline
 
 from die import Die
 
-# Create a D6
-die = Die()
+# Create two D6 dice
+die_1 = Die()
+die_2 = Die()
 
 # Make some rolls and store the values
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze the results
 frequencies = []
-for value in range(1, die.num_sides + 1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results
-x_values = list(range(1, die.num_sides + 1))
+x_values = list(range(2, max_result + 1))
 data = [Bar(x = x_values, y = frequencies)]
 
 # Configure the axes
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
 my_layout = Layout(
-    title='Results of Rolling One D6 1000 Times',
+    title='Results of Rolling Two D6 Dice 1000 Times',
     xaxis = x_axis_config,
     yaxis = y_axis_config
 )
 
 offline.plot(
     {'data': data, 'layout':my_layout},
-    filename='d6.html'
+    filename='d6_d6.html'
 )
